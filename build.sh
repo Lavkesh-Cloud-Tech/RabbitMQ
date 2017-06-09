99068caf6df3426fda55489c4ed8c4feb88a64b6
Lavkesh <l.rathi@elsevier.com>
2017-06-09 09:02:25 -0400

    rabbitmq portability

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 
+echo "======================== Start build image ======================="
 docker build -t lavkesh/rabbitmq:$1 .
+echo "Image build => lavkesh/rabbitmq:$1"
--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 set -e
 
-APP_NAME=lavkesh-rabbitmq
-echo "$APP_NAME"
-CONTAINER_ID=$(docker ps -a -f name="$APP_NAME" -q)
+APP_NAME=rabbitmq
+NETWORK_NAME=my-bridge
+echo "Container Name: $APP_NAME"
+echo "Network: $NETWORK_NAME"
 
-if [[ $? -eq 0 ]]; then
-   if [[ $CONTAINER_ID != "" ]]; then
-    echo "Removing CONTAINER_ID  =>  $CONTAINER_ID"
-    docker stop $CONTAINER_ID
-    docker rm -f $CONTAINER_ID
-   fi
-else
-  #Fail
-  echo "'docker ps -a -f name=$APP_NAME -q' command not run successfully!"
-  exit 1
+NETWORK_ID=$(docker network ls -f "name=$NETWORK_NAME" -q)
+if [[ $NETWORK_ID = "" ]]; then
+  echo "Creating network  $NETWORK_NAME"
+  docker network create -d bridge $NETWORK_NAME
 fi
 
-docker run --hostname $APP_NAME --name $APP_NAME \
+CONTAINER_ID=$(docker ps -a -f name="$APP_NAME" -q)
+if [[ $CONTAINER_ID != "" ]]; then
+  echo "Removing CONTAINER_ID  =>  $CONTAINER_ID"
+  docker stop $CONTAINER_ID
+  docker rm -f $CONTAINER_ID
+fi
+
+docker run -d --name $APP_NAME \
+--network=$NETWORK_NAME \
 -p 15672:15672 -p 5672:5672 \
 -e RABBITMQ_USERNAME=rabbitmq-user \
 -e RABBITMQ_PASSWORD=LwXIYhhspbOomXmLfibRGzOfumBRNe96OX5jqWuQMl+uuIxY \
